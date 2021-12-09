fd7ee23976b9483e3143398ae541382f273ad2c8
Matheus Valença <82548455+Matheus351@users.noreply.github.com>
2021-12-08 22:24:56 -0300

    adicionando questão 7

---
+++ questao07.py
@@ -0,0 +1,9 @@
+n1 = int(input("Informe um número: "))
+n2 = int(input("Informe mais um número: "))
+
+
+resultado = 0
+for i in range(n1):
+    resultado += n2
+else:
+    print("O resultado é:%.2f" %resultado)
